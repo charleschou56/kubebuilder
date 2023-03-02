74df06ed2c4c6084ca4bc3b1e87a3fc69301afeb
Charles Chou <charles_chou@accton.com>
2023-03-02 10:06:27 +0000

    Add business logic

--- api/v1alpha1/at_types.go
+++ api/v1alpha1/at_types.go
@@ -22,19 +22,32 @@ import (
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+const (
+	PhasePending = "PENDING"
+	PhaseRunning = "RUNNING"
+	PhaseDone    = "DONE"
+)
 
 // AtSpec defines the desired state of At
 type AtSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of At. Edit at_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Schedule is the desired time the command is supposed to be executed.
+	// Note: the format used here is UTC time https://www.utctime.net
+	Schedule string `json:"schedule,omitempty"`
+	// Command is the desired command (executed in a Bash shell) to be executed.
+	Command string `json:"command,omitempty"`
+	// Important: Run "make" to regenerate code after modifying this file
 }
 
 // AtStatus defines the observed state of At
 type AtStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	// Phase represents the state of the schedule: until the command is executed
+	// it is PENDING, afterwards it is DONE.
+	Phase string `json:"phase,omitempty"`
 	// Important: Run "make" to regenerate code after modifying this file
 }
 
